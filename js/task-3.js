cf7175408833d0b9c8186afc473c2e9b3e82b031
mykytaterentiev <mykytaterentievua@gmail.com>
2023-11-14 15:27:40 +0100

    14.11

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,12 @@
-function getElementWidth(content, padding, border) {
-const contentWidth = parseFloat(content);
-  const paddingWidth = parseFloat(padding);
-  const borderWidth = parseFloat(border);
-  const totalWidth = contentWidth + 2 * (paddingWidth + borderWidth);
+function checkForSpam(message) {
+    const messageLower = message.toLowerCase();
+    return messageLower.includes(`sale`) || messageLower.includes(`buy`);
+}
 
-  return totalWidth;
-};
-
-console.log(getElementWidth("50px", "8px", "4px")); 
-console.log(getElementWidth("60px", "12px", "8.5px")); 
-console.log(getElementWidth("200px", "0px", "0px")); 
+console.log(checkForSpam("Latest technology news")); 
+console.log(checkForSpam("JavaScript weekly newsletter"));
+console.log(checkForSpam("Get best sale offers now!")); 
+console.log(checkForSpam("Amazing SalE, only tonight!"));
+console.log(checkForSpam("Trust me, this is not a spam message")); 
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); 
+console.log(checkForSpam("[SPAM] How to earn fast money?")); 
